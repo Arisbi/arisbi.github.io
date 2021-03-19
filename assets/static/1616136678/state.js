8a91e0e14a972d417a0357217a793406d436a126
Arisbi <Arisbi@users.noreply.github.com>
2021-03-19 06:51:21 +0000

    deploy: 65663da71aa61d5087b67e3e5d947f8ac9bf0bdb

---
+++ assets/static/1616136678/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcharisbi\u002Fassets\u002Fstatic\u002F1616136678",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fcharisbi\u002F",assetsPath:"\u002Fcharisbi\u002Fassets\u002F",cdnURL:a}}}}(null));
