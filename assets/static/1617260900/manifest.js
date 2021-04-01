81c85e5a71dff9013cf43948d6960a7c8bb7042d
Arisbi <arisbi@outlook.com>
2021-04-01 01:08:38 -0600

    Updates

---
+++ assets/static/1617260900/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fen\u002Fillustrations","\u002Fcurriculumvitae","\u002Fillustrations","\u002Fphotography","\u002Fmotiongraphics","\u002Fprojects","\u002Fen\u002Fmotiongraphics","\u002Fen\u002Fphotography","\u002Fwebdesign","\u002Fen\u002Fcurriculumvitae","\u002Fen\u002Fprojects","\u002Fen\u002Fwebdesign","\u002Fen"]})
